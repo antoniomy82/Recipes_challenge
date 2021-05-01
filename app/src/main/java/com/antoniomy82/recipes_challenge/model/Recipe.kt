0bf43948dba051762a56250299a544cdfa672744
Antonio <antoniomy82@gmail.com>
2021-05-01 13:26:51 +0200

    Initial commit

---
+++ app/src/main/java/com/antoniomy82/recipes_challenge/model/Recipe.kt
@@ -0,0 +1,8 @@
+package com.antoniomy82.recipes_challenge.model
+
+data class Recipe(
+    var tittle: String ?="",
+    var href: String ?="",
+    var ingredients: String ?="",
+    var thumbail: String ?=""
+)
---
+++ app/src/main/java/com/antoniomy82/recipes_challenge/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.antoniomy82.recipes_challenge.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.antoniomy82.recipes_challenge.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
